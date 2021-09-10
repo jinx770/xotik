2afab2c9a80f74b42fac1ade0c3269cc8dd5797e
jinx770 <61133151+jinx770@users.noreply.github.com>
2021-09-10 15:55:08 +1200

    commented every line

--- frontEnd/js/listingHandler.js
+++ frontEnd/js/listingHandler.js
@@ -14,33 +14,57 @@
 
 // ----------------------------------------------------------------------------------------------------------------------------------
 
-
+    // Function that gets called when you click on the + icon
     let handleFileSelect = (e) => {
 
+        // Loop every .5 seconds
         setInterval(() => {
-             if (cache.includes(fileInput.value)) {
-                 return
-             }
 
-             if (!cache[4]) {
+            // Checks the entered input of the file popup with items in the cached array
+            // Purely to avoid spam pushes to the array
+            if (cache.includes(fileInput.value)) {
 
+                // Return stops the current thread from continuing
+                return
+
+            }
+
+            // Quick check to make sure the count of the cached array isn't over 5
+            if (!cache[4]) {
+
+                // Gets the inputted file of our input
                 let file = $("input[type=file]").get(0).files[0];
+
+                // Adds the name of the value to our cache array
+                // Making sure we aren't uploading the same image
                 cache.push(fileInput.value)
 
+                // Making sure file has been successfully defined
                 if (file) {
 
-                  let reader = new FileReader();
-                  reader.onload = function(){
-                      imageHolder.innerHTML += `<img onclick="remove(this)" id=${fileInput.value} src="${reader.result}" alt="">`
-                  }
+                    // Dependancy for rendering a file using a base64 converter.
+                    // With this we can create images straight from our files!
+                    // Which normally wouldn't be allowed by googles security zz
+                    let reader = new FileReader();
 
-                  reader.readAsDataURL(file);
+                    // Event handler for when load is fired
+                    reader.onload = function(){
 
-                  console.log(cache)
+                        // Creating a new image card with its own onclick function to remove itself if you click it
+                        // Makes the id of the card, the name of the image so we can remove it later on from the cache array
+                        // The source attribute gets changed to the result of our fileReaders request when we call it with readAsDataURL
+                        imageHolder.innerHTML += `<img onclick="remove(this)" id=${fileInput.value} src="${reader.result}" alt="">`
+
+                    }
+
+                    // Calling our file reader function with the file we saved early as an argument
+                    reader.readAsDataURL(file);
+
                 }
 
             }
 
+        // Running every .5 seconds
         }, 500)
 
         if (cache[4]) {
@@ -55,9 +79,10 @@
 // ----------------------------------------------------------------------------------------------------------------------------------
 
 
-
+    // Array remove function
+    // It's some online function I found, didn't make it
     function removeA(arr) {
-        var what, a = arguments, L = a.length, ax;
+        let what, a = arguments, L = a.length, ax;
         while (L > 1 && arr.length) {
             what = a[--L];
             while ((ax= arr.indexOf(what)) !== -1) {
@@ -73,13 +98,17 @@
 
 
 
+    // Remove function that gets called if you click on the image
     let remove = (el) => {
 
+        // Irrelevant defining, didn't really needa do this but shrug
         let element = el;
-        console.log(cache)
 
-        cache.includes(element.id) ? removeA(cache, element.id) : "asd"
+        // Checking to see if the image we're removing, is cached
+        // Which it will always be so we remove it using our remove array function with the ID we set in the image handler function
+        cache.includes(element.id) ? removeA(cache, element.id) : null
 
+        // Removes the element
         element.remove();
 
     }
@@ -90,7 +119,15 @@
 
 
 
+    // Little slight of hand function, since I didn't want to replace Toby's button
+    // I hid an invisible input behind his button, buttons don't have a type attribute but inputs do,
+    // When you click his button I force click the input which opens up the files :D
     addImageListingButton.addEventListener('click', () => {
+
+        // Click event
         fileInput.click()
+
+        // Function for handling our images
         handleFileSelect()
+        
     })
